90cea34be23de63c7d35235899572a6b8cb3953c
戴晓波 <daixiaobo@wondersoft.cn>
2019-07-22 17:22:18 +0800

    加了个获取所有requestMapping路径方法

--- src/main/java/com/dxb/swagger/SwaggerApplication.java
+++ src/main/java/com/dxb/swagger/SwaggerApplication.java
@@ -1,9 +1,12 @@
 package com.dxb.swagger;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,12 +15,20 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Map;
+
 @SpringBootApplication
 @EnableSwagger2
 public class SwaggerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SwaggerApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SwaggerApplication.class, args);
+        RequestMappingHandlerMapping rmhp = applicationContext.getBean(RequestMappingHandlerMapping.class);
+        Map<RequestMappingInfo, HandlerMethod> map = rmhp.getHandlerMethods();
+        for (RequestMappingInfo info : map.keySet()) {
+            //获取所有RequestMapping路径
+            System.out.println(info.getPatternsCondition().toString().replace("[", "").replace("]", ""));
+        }
     }
 
     @Bean
